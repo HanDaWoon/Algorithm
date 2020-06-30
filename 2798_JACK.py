1d72aba436e7a710482ff7f186087ed69a1134d1
HanDaWoon <bookpia.han@gmail.com>
2020-06-30 14:15:21 +0000

    BOJ 2020.06 ~ 2020.07

--- 2798_JACK.py
+++ 2798_JACK.py
@@ -3,24 +3,17 @@ import sys
 cnt = 0
 tmp = 0
 temp = 0
-check = False
 
 N, M = map(int, sys.stdin.readline().split())
 cards = list(map(int, sys.stdin.readline().split()))
 
-while check == True:
+for k in range(N):
     card_list = cards.copy()    #리스트 복제
     list_len = len(card_list)
-    exceptt = card_list.pop(cnt)    #더해줄 카드값 1개 빼줌
-
+    exceptt = card_list.pop(k)    #더해줄 카드값 1개 빼줌
     for i in range(list_len):
         for j in card_list[i+1:list_len]:   
-            temp = i+j+exceptt
-            print(temp)
-            if temp > tmp and temp <= M:
+            temp = card_list[i]+j+exceptt
+            if temp >= tmp and temp <= M:
                 tmp = temp
-            elif temp == M:
-                print(temp)
-                exit()
-    print(tmp)
-    cnt += 1
+print(tmp)
