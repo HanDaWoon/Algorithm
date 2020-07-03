5a1977256e1ea3c17b94715f09c5b5288d5fad35
한다운 <35947660+HanDaWoon@users.noreply.github.com>
2020-07-03 23:54:29 +0900

    BOJ 2020.07 ~ 2020.08 (#5)

--- 1463_MakeIt1.py
+++ 1463_MakeIt1.py
@@ -1,14 +1,20 @@
 X = int(input())
 
-dynamic = [0 for _ in range(X+1)]    #크기 X+1dml 0 으로 된 리스트 생성
+"""
+점화식
+f(n)=f(n-1)+1
+    =f(n/2)+1   if n%2 == 0
+    =f(n/3)+1   if n%3 == 0
+"""
+dynamic = [0 for _ in range(X+1)]
 
-for i in range(2, X+1): 
-    dynamic[i] = dynamic[i-1] + 1  
+for i in range(2, X+1):
+    dynamic[i] = dynamic[i-1] + 1
 
-    if i%2 == 0 and dynamic[i] > dynamic[i//2] + 1 :
+    if i%2 == 0 and dynamic[i] > dynamic[i//2] + 1:
         dynamic[i] = dynamic[i//2]+1
         
-    if i%3 == 0 and dynamic[i] > dynamic[i//3] + 1 :
+    if i%3 == 0 and dynamic[i] > dynamic[i//3] + 1:
         dynamic[i] = dynamic[i//3] + 1
-        
+
 print(dynamic[X])
