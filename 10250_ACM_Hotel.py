731bd8b7d9d4391e77b2a5b764133047a36a46af
한다운 <35947660+HanDaWoon@users.noreply.github.com>
2020-08-09 22:57:20 +0900

    Revert "BOJ 2020.07 2020.08"

--- 10250_ACM_Hotel.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for _ in range(int(sys.stdin.readline())):
-    H, W, N = map(int, sys.stdin.readline().split())
-    if N%H == 0:
-        print((H*100)+(N//H))
-    else:
-        print((N%H*100)+(N//H+1))
--- 1085_EscapeRact.py
+++
@@ -1,5 +0,0 @@
-x, y, w, h = map(int, input().split())
-
-a, b = (w-x), (h-y)
-
-print(min(a,b,x,y))
